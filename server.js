cd4624b82b3ba63e3a50f1f2d3f2567d8b423168
Quinesha <qtiemations@gmail.com>
2023-05-11 17:24:54 +0200

    'commit'

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ app.engine("handlebars", engine({ defaultLayout:"main"}))
 app.set("view engine", "handlebars")
 app.set("views", "views")
 
-app.listen(3000, () => {
-	console.log("server is at port 3000", 3000)
+app.listen(8080, () => {
+	console.log("server is at port 8080", 8080)
 })
 
 //routing
