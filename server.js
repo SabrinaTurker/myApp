4a1d361d3871d29e2f924bf2bf2530f7d51bc784
Quinesha <qtiemations@gmail.com>
2023-05-11 18:30:47 +0200

    commit

--- server.js
+++ server.js
@@ -6,6 +6,8 @@ app.engine("handlebars", engine({ defaultLayout:"main"}))
 app.set("view engine", "handlebars")
 app.set("views", "views")
 
+app.use(express.static("public"))
+
 app.listen(8080, () => {
 	console.log("server is at port 8080", 8080)
 })
@@ -13,4 +15,9 @@ app.listen(8080, () => {
 //routing
 app.get("/", (req, res) => {
 	res.render("index")
+	//homepage
+})
+
+app.get('/login', (req, res) => {
+    // login.handlebars
 })
