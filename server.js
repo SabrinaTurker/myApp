940fc923937b9eb4b0c41c964261861f70653427
Quinesha <qtiemations@gmail.com>
2023-05-12 16:04:05 +0200

    commit

--- server.js
+++ server.js
@@ -2,36 +2,36 @@ const express = require("express")
 const app = express()
 
 const { engine } = require("express-handlebars")
-app.engine("handlebars", engine({ defaultLayout:"main"}))
+app.engine("handlebars", engine({ defaultLayout: "main" }))
 app.set("view engine", "handlebars")
 app.set("views", "views")
 
 app.use(express.static("public"))
 
-app.use(express.urlencoded({ extended: true })) 
+app.use(express.urlencoded({ extended: true }))
 // to parse the form data
 
 app.listen(8080, () => {
-	console.log("server is at port 8080", 8080)
+    console.log("server is at port 8080", 8080)
 })
 
 //routing
 app.get("/", (req, res) => {
-	res.render("index")
-	//homepage
+    res.render("index")
+    //homepage
 })
 
-app.get('/guest', (req, res) => {
-	res.render("login",  { message: ""})
-	//loginpage versie 2
+app.get("/guest", (req, res) => {
+    res.render("login", { message: "" })
+    //loginpage versie 2
 })
 
-app.post('/login', (req, res) => {
+app.post("/login", (req, res) => {
     // const { username, password } = req.body
     // check if everything is correct
     if (req.body.username === "Quinesha" && req.body.password === "meep") {
-        res.render('login')
+        res.render("login")
     } else {
-        res.render('error', { message: "Invalid username or password" })
+        res.render("error", { message: "Invalid username or password" })
     }
 })
